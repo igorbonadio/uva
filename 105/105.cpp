515e0b3a33a6f3ab0b6d02436e2dc7a0c2ab2035
Ígor Bonadio <igorbonadio@gmail.com>
2011-07-25 10:03:50 -0300

    105 - The Skyline Problem

---
+++ 105/105.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void PutBuild(int height[], int a, int h, int b) {
+  for (int i = a; i < b; i++)
+    if (height[i] < h)
+      height[i] = h;
+}
+
+void Syline(int height[], int max_x) {
+  int h = 0;
+  for (int i = 0; i <= max_x; i++) {
+    if (height[i] != h) {
+      h = height[i];
+      cout << i << " " << h << (max_x == i ? "" : " ");
+    }
+  }
+  cout << endl;
+}
+
+int main() {
+  int height[10001] = {0};
+  int a, h, b, max_x = 0;
+  while (cin >> a >> h >> b) {
+    PutBuild(height, a, h, b);
+    if (b > max_x)
+      max_x = b;
+  }
+  Syline(height, max_x);
+  return 0;
+}
