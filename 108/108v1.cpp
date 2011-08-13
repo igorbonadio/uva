58026848891fcbb3ea25bb0fb8f3f4f76c3ff077
Ígor Bonadio <igorbonadio@gmail.com>
2011-08-12 23:12:35 -0300

    removing tabs...

--- 108/108v1.cpp
+++ 108/108v1.cpp
@@ -5,48 +5,48 @@
 using namespace std;
 
 int main() {
-	int v;
-	int n;
-	int max = INT_MIN;
-	vector<vector<vector<vector<int > > > > s(100, vector<vector<vector<int > > >(100, vector<vector<int > >(100, vector<int >(100))));
+  int v;
+  int n;
+  int max = INT_MIN;
+  vector<vector<vector<vector<int > > > > s(100, vector<vector<vector<int > > >(100, vector<vector<int > >(100, vector<int >(100))));
 
-	while (cin >> n) {
+  while (cin >> n) {
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				cin >> v;
-				s[i][j][i][j] = v;
-				if (v > max)
-					max = v;
-			}
-		}
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int k = i+1; k < n; k++) {
-					v = s[i][j][k-1][j] + s[k][j][k][j];
-					s[i][j][k][j] = v;
-					if (v > max)
-						max = v;
-				}
-			}
-		}
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int k = i; k < n; k++) {
-					for (int l = j+1; l < n; l++) {
-						v = s[i][j][k][l-1] + s[i][l][k][l];
-						s[i][j][k][l] = v;
-						if (v > max)
-							max = v;
-					}
-				}
-			}
-		}
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        cin >> v;
+        s[i][j][i][j] = v;
+        if (v > max)
+          max = v;
+      }
+    }
+    
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        for (int k = i+1; k < n; k++) {
+          v = s[i][j][k-1][j] + s[k][j][k][j];
+          s[i][j][k][j] = v;
+          if (v > max)
+            max = v;
+        }
+      }
+    }
+    
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        for (int k = i; k < n; k++) {
+          for (int l = j+1; l < n; l++) {
+            v = s[i][j][k][l-1] + s[i][l][k][l];
+            s[i][j][k][l] = v;
+            if (v > max)
+              max = v;
+          }
+        }
+      }
+    }
 
-		cout << max << endl;
-	}
+    cout << max << endl;
+  }
 
-	return 0;
+  return 0;
 }
--- 108/108v2.cpp
+++ 108/108v2.cpp
@@ -5,46 +5,46 @@
 using namespace std;
 
 int main() {
-	int v;
-	int n;
-	int max = INT_MIN;
-	vector<vector<int > > s(101, vector<int > (101, 0));
+  int v;
+  int n;
+  int max = INT_MIN;
+  vector<vector<int > > s(101, vector<int > (101, 0));
 
-	while (cin >> n) {
+  while (cin >> n) {
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j < n; j++) {
-				cin >> v;
-				s[i][j] = v;
-				if (v > max)
-					max = v;
-			}
-		}
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < n; j++) {
+        cin >> v;
+        s[i][j] = v;
+        if (v > max)
+          max = v;
+      }
+    }
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j < n; j++) {
-				v = s[i-1][j] + s[i][j];
-				s[i][j] = v;
-				if (v > max)
-					max = v;
-			}
-		}
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < n; j++) {
+        v = s[i-1][j] + s[i][j];
+        s[i][j] = v;
+        if (v > max)
+          max = v;
+      }
+    }
 
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int k = i+1; k <= n; k++) {
-					v = 0;
-					for (int l = j; l < n; l++) {
-						v += s[k][l] - s[i][l];
-						if (v > max)
-							max = v;
-					}
-				}
-			}
-		}
+    for (int i = 0; i <= n; i++) {
+      for (int j = 0; j < n; j++) {
+        for (int k = i+1; k <= n; k++) {
+          v = 0;
+          for (int l = j; l < n; l++) {
+            v += s[k][l] - s[i][l];
+            if (v > max)
+              max = v;
+          }
+        }
+      }
+    }
 
-		cout << max << endl;
-	}
+    cout << max << endl;
+  }
 
-	return 0;
+  return 0;
 }
